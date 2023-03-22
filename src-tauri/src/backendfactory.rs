d0d25bc89711e73147b6c37c5c43f84e13743b0e
bitfl0wer <florian@pro-weber.com>
2023-03-22 19:11:53 +0100

    self not needed, add imports

--- src-tauri/src/backendfactory.rs
+++ src-tauri/src/backendfactory.rs
@@ -1,10 +1,18 @@
-struct BackendFactory {}
+pub mod backend_factory {
+    use crate::backend::backend;
+    use crate::instance::instance::InstanceType;
+    pub struct BackendFactory {}
 
-impl BackendFactory {
-    pub fn create_backend(&self, instance: backend::InstanceType) -> impl backend::Backend {
-        match instance {
-            InstanceType::Fosscord => backend::FosscordBackend::new(),
-            InstanceType::Discord => backend::DiscordBackend::new(),
+    /// The BackendFactory handles additional logic for backend instance creation.
+    /// For example, it creates a new backend instance based on the instance type.
+    impl BackendFactory {
+        pub fn create_backend(instance_type: InstanceType) -> impl backend::Backend {
+            let backend: backend::Backend;
+            match instance_type {
+                InstanceType::Fosscord => backend = backend::FosscordBackend::new(),
+                InstanceType::Discord => backend = backend::DiscordBackend::new(),
+            }
+            backend
         }
     }
 }
