010acaf8070f6a00a46ce51cf2f53a6124fc6983
bitfl0wer <florian@pro-weber.com>
2023-03-26 23:40:19 +0200

    Implement a register function for Fosscord

--- src-tauri/src/auth.rs
+++ src-tauri/src/auth.rs
@@ -1,5 +1,9 @@
 pub mod auth {
-    use crate::backend::backend::Backend;
+    use crate::backend::backend::{Backend, FosscordBackend};
+    use reqwest::{Client, Error, RequestBuilder, Response};
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
     pub struct RegisterParams {
         email: String,
         password: String,
@@ -13,6 +17,7 @@ pub mod auth {
         promotional_email_opt_in: bool,
     }
 
+    #[derive(Debug, Serialize, Deserialize)]
     pub struct LoginParams {
         login: String,
         password: String,
@@ -22,11 +27,30 @@ pub mod auth {
         gift_code_sku_id: Option<String>,
     }
 
-    pub async fn register(backend_object: &impl Backend, params: RegisterParams) -> String {
-        return String::from("Hello"); //TODO: Implement this
+    pub async fn register_fosscord(
+        backend_object: &FosscordBackend,
+        params: RegisterParams,
+    ) -> String {
+        let json: String = serde_json::to_string(&params).unwrap();
+        let client: &Client = &backend_object.http_client;
+        let request: RequestBuilder = client
+            .post(backend_object.instance_url.clone() + "/api/register/")
+            .body(json);
+        let result: Result<Response, Error> = request.send().await;
+        match result {
+            Ok(result) => match result.text().await {
+                Ok(body) => body,
+                Err(error) => {
+                    panic!("Something went wrong. {}", error);
+                }
+            },
+            Err(error) => {
+                panic!("An error occured while registering. {}", error)
+            }
+        }
     }
 
-    pub async fn login(backend_object: &impl Backend, params: LoginParams) -> String {
+    pub async fn login_fosscord(backend_object: &FosscordBackend, params: LoginParams) -> String {
         return String::from("Hello"); //TODO: Implement this
     }
 }
