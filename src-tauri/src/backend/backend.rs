c5baad67442531f5fe2eee9c43a78219ddd3bb50
bitfl0wer <florian@pro-weber.com>
2023-04-03 17:35:13 +0200

    URL Parsing is now part of URLBundle

--- src-tauri/src/backend/backend.rs
+++ src-tauri/src/backend/backend.rs
@@ -1,13 +1,36 @@
 pub mod backend {
+    use url::{ParseError, Url};
 
     #[derive(Clone, Default)]
     pub struct URLBundle {
-        api: String,
-        wss: String,
-        cdn: String,
+        pub api: String,
+        pub wss: String,
+        pub cdn: String,
     }
 
     impl URLBundle {
+        pub fn new(api: String, wss: String, cdn: String) -> Self {
+            Self {
+                api: URLBundle::parse_url(api),
+                wss: URLBundle::parse_url(wss),
+                cdn: URLBundle::parse_url(cdn),
+            }
+        }
+
+        /// parse(url: String) parses a URL using the Url library and formats it in a standardized
+        /// way: http(s)://subdomain.domain.toplevel
+        pub fn parse_url(url: String) -> String {
+            let url = match Url::parse(&url) {
+                Ok(url) => url,
+                Err(ParseError::RelativeUrlWithoutBase) => {
+                    let url = format!("http://{}", url);
+                    Url::parse(&url).unwrap()
+                }
+                Err(_) => panic!("Invalid URL"),
+            };
+            url.to_string()
+        }
+
         pub fn get_api(&self) -> &str {
             &self.api
         }
@@ -20,6 +43,7 @@ pub mod backend {
             &self.wss
         }
     }
+
     #[async_trait::async_trait]
     pub trait Backend {
         /// The backend trait will define all needed functions/behaviour for the client to
