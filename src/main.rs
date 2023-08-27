93137b4844249a49c196d7ddf2d437715689f7a0
bitfl0wer <florian@pro-weber.com>
2023-08-27 18:26:27 +0200

    something something login

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,70 @@
 use chorus::instance::Instance;
 use chorus::types::RegisterSchema;
 use chorus::UrlBundle;
+use iced::futures::executor::block_on;
 use iced::widget::{button, column, text};
 use iced::{Alignment, Element, Sandbox, Settings};
 
 #[tokio::main]
 async fn main() -> iced::Result {
-    let urls = UrlBundle::new(
-        "http://localhost:3001/api".to_string(),
-        "ws://localhost:3001".to_string(),
-        "http://localhost:3001".to_string(),
-    );
-    let mut instance = Instance::new(urls, false).await.unwrap();
-    let user = instance
-        .register_account(&RegisterSchema {
-            username: "Flori on Polyphony".to_string(),
-            consent: true,
-            date_of_birth: Some("1999-01-01".to_string()),
-            ..Default::default()
-        })
-        .await
-        .unwrap();
     Counter::run(Settings::default())
 }
 
 struct Counter {
-    value: i32,
+    username: String,
+    discrim: String,
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
     IncrementPressed,
-    DecrementPressed,
 }
 
 impl Sandbox for Counter {
     type Message = Message;
 
     fn new() -> Self {
-        Self { value: 0 }
+        Self {
+            username: "Not logged in".to_string(),
+            discrim: "#0000".to_string(),
+        }
     }
 
     fn title(&self) -> String {
-        String::from("Counter - Iced")
+        String::from("client")
     }
 
     fn update(&mut self, message: Message) {
         match message {
             Message::IncrementPressed => {
-                self.value += 1;
-            }
-            Message::DecrementPressed => {
-                self.value -= 1;
+                block_on(async {
+                    let urls = UrlBundle::new(
+                        "http://localhost:3001/api".to_string(),
+                        "ws://localhost:3001".to_string(),
+                        "http://localhost:3001".to_string(),
+                    );
+                    let mut instance = Instance::new(urls, false).await.unwrap();
+                    let user = instance
+                        .register_account(&RegisterSchema {
+                            username: "Flori on Polyphony".to_string(),
+                            consent: true,
+                            date_of_birth: Some("1999-01-01".to_string()),
+                            ..Default::default()
+                        })
+                        .await
+                        .unwrap();
+                    self.username = user.object.read().unwrap().username.clone();
+                    self.discrim =
+                        "#".to_string() + user.object.read().unwrap().discriminator.as_str();
+                });
             }
         }
     }
 
     fn view(&self) -> Element<Message> {
         column![
-            button("Increment").on_press(Message::IncrementPressed),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::DecrementPressed)
+            text(self.username.clone() + &self.discrim.clone()).size(20),
+            button("Create account").on_press(Message::IncrementPressed),
         ]
         .padding(20)
         .align_items(Alignment::Center)
