7ae71294160c53834f4533d812bf8eb41de7fa45
bitfl0wer <florian@pro-weber.com>
2023-08-28 20:30:19 +0200

    progress

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use chorus::instance::{ChorusUser, Instance};
 use chorus::types::RegisterSchema;
 use chorus::UrlBundle;
 use iced::futures::executor::block_on;
-use iced::widget::{button, column, text};
-use iced::{Alignment, Application, Command, Element, Settings};
+use iced::widget::{button, column, container, text};
+use iced::{Alignment, Application, Command, Element, Length, Settings};
 
 #[tokio::main]
 async fn main() -> iced::Result {
@@ -15,11 +15,12 @@ enum Client {
     Connected(Polyphony),
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 enum Message {
     GuildJoined,
     MessageReceived,
-    Connect,
+    Connect((UrlBundle, RegisterSchema)),
+    Connected(Polyphony),
 }
 
 impl Application for Client {
@@ -32,40 +33,66 @@ impl Application for Client {
     type Flags = ();
 
     fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
-        let urls = UrlBundle::new(
-            "http://localhost:3001/api".to_string(),
-            "ws://localhost:3001/".to_string(),
-            "http://localhost:3001/".to_string(),
-        );
-        let register = RegisterSchema {
-            username: "userrrrrr".to_string(),
-            password: Some("test".to_string()),
-            consent: true,
-            ..Default::default()
-        };
-        (
-            Client::Disconnected,
-            Command::perform(Polyphony::new(urls, register), |_| ()),
-        )
+        (Client::Disconnected, Command::none())
     }
 
     fn title(&self) -> String {
-        todo!()
+        let subtitle = match self {
+            Client::Disconnected => "Disconnected",
+            Client::Connected(_) => "Connected",
+        };
+
+        format!("Client - {}", subtitle)
     }
 
     fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
-        todo!()
+        match message {
+            Message::Connect((urls, register)) => {
+                Command::perform(Polyphony::new(urls, register), Message::Connected)
+            }
+            _ => Command::none(),
+        }
     }
 
     fn view(&self) -> Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
-        todo!()
+        let content = match self {
+            Client::Connected(instance) => {
+                column!(text(format!(
+                    "Connected with token {}",
+                    instance.user.token()
+                )))
+            }
+            Client::Disconnected => {
+                let urls = UrlBundle::new(
+                    "http://localhost:3001/api".to_string(),
+                    "ws://localhost:3001/".to_string(),
+                    "http://localhost:3001/".to_string(),
+                );
+                let register = RegisterSchema {
+                    username: "userrrrrr".to_string(),
+                    password: Some("test".to_string()),
+                    consent: true,
+                    ..Default::default()
+                };
+                column!(
+                    text("Disconnected"),
+                    button("Connect").on_press(Message::Connect((urls, register)))
+                )
+            }
+        };
+        container(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
     }
 }
 
 #[derive(Debug)]
 struct Polyphony {
-    instance: Instance,
-    user: ChorusUser,
+    pub instance: Instance,
+    pub user: ChorusUser,
 }
 
 impl Polyphony {
