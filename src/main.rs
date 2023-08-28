22acfd8134c641eb5acaf09bd0c0b2b2cd56981f
bitfl0wer <florian@pro-weber.com>
2023-08-28 18:27:50 +0200

    New Application time

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,77 @@
-use chorus::instance::Instance;
+use chorus::instance::{ChorusUser, Instance};
 use chorus::types::RegisterSchema;
 use chorus::UrlBundle;
 use iced::futures::executor::block_on;
 use iced::widget::{button, column, text};
-use iced::{Alignment, Element, Sandbox, Settings};
+use iced::{Alignment, Application, Command, Element, Settings};
 
 #[tokio::main]
 async fn main() -> iced::Result {
-    Counter::run(Settings::default())
+    Client::run(Settings::default())
 }
 
-struct Counter {
-    username: String,
-    discrim: String,
+enum Client {
+    Disconnected,
+    Connected(Polyphony),
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    IncrementPressed,
+    GuildJoined,
+    MessageReceived,
+    Connect,
 }
 
-impl Sandbox for Counter {
+impl Application for Client {
+    type Executor = iced::executor::Default;
+
     type Message = Message;
 
-    fn new() -> Self {
-        Self {
-            username: "Not logged in".to_string(),
-            discrim: "#0000".to_string(),
-        }
+    type Theme = iced::Theme;
+
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
+        let urls = UrlBundle::new(
+            "http://localhost:3001/api".to_string(),
+            "ws://localhost:3001/".to_string(),
+            "http://localhost:3001/".to_string(),
+        );
+        let register = RegisterSchema {
+            username: "userrrrrr".to_string(),
+            password: Some("test".to_string()),
+            consent: true,
+            ..Default::default()
+        };
+        (
+            Client::Disconnected,
+            Command::perform(Polyphony::new(urls, register), |_| ()),
+        )
     }
 
     fn title(&self) -> String {
-        String::from("client")
+        todo!()
     }
 
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::IncrementPressed => {
-                block_on(async {
-                    let urls = UrlBundle::new(
-                        "http://localhost:3001/api".to_string(),
-                        "ws://localhost:3001".to_string(),
-                        "http://localhost:3001".to_string(),
-                    );
-                    let mut instance = Instance::new(urls, false).await.unwrap();
-                    let user = instance
-                        .register_account(&RegisterSchema {
-                            username: "Flori on Polyphony".to_string(),
-                            consent: true,
-                            date_of_birth: Some("1999-01-01".to_string()),
-                            ..Default::default()
-                        })
-                        .await
-                        .unwrap();
-                    self.username = user.object.read().unwrap().username.clone();
-                    self.discrim =
-                        "#".to_string() + user.object.read().unwrap().discriminator.as_str();
-                });
-            }
-        }
+    fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
+        todo!()
     }
 
-    fn view(&self) -> Element<Message> {
-        column![
-            text(self.username.clone() + &self.discrim.clone()).size(20),
-            button("Create account").on_press(Message::IncrementPressed),
-        ]
-        .padding(20)
-        .align_items(Alignment::Center)
-        .into()
+    fn view(&self) -> Element<'_, Self::Message, iced::Renderer<Self::Theme>> {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+struct Polyphony {
+    instance: Instance,
+    user: ChorusUser,
+}
+
+impl Polyphony {
+    async fn new(urls: UrlBundle, register: RegisterSchema) -> Self {
+        let mut instance = Instance::new(urls, false).await.unwrap();
+        let user = instance.register_account(&register).await.unwrap();
+        Self { instance, user }
     }
 }
